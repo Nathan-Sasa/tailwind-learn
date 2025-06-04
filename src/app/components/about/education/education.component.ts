176a7dd3caffac3d28e70b848336de440fc58d82
Nathan-Sasa <Jonathanbikuta05@gmail.com>
2025-06-04 23:17:38 +0100

    customisation des cartes des informations sur l'education

--- src/app/components/about/education/education.component.ts
+++ src/app/components/about/education/education.component.ts
@@ -16,16 +16,24 @@ export class EducationComponent {
 
     educations = [
         {
-            icon              :  '',
-            grad              :  '',
-            fac               :  '',
-            university      :  ''
+            icon              :  'school',
+            iconLocation  :  'location_pin',
+            location        : 'Institut Supérieur d\'informatique Programmation et Analyse/Kinshasa',
+            grad              :  'Licence en Sciences Informatiques',
+            fac               :  'Administration Réseaux et Télécommunications',
+            university      :  'nstitut Supérieur – Kinshasa', 
+            class             :  ' bg-gradient-to-br from-pink-50 to-pink-100',
+            description     :  'Formation approfondie en informatique avec spécialisation en administration des réseaux et télécommunications. Acquisition de compétences solides en programmation, gestion de bases de données, sécurité informatique et architecture des systèmes d\'information.'
         },
         {
-            icon              :  '',
-            grad              :  '',
-            fac               :  '',
-            university      :  ''
+            icon              :  'school',
+            iconLocation  :  'location_pin',
+            location        : 'Institut Supérieur d\'informatique Programmation et Analyse/Kinshasa',
+            grad              :  'Graduat en Informatique Industrielle',
+            fac               :  'Formation technique spécialisée',
+            university      :  'Institut Technique – Kinshasa',
+            class             :  ' bg-gradient-to-br from-pink-50 to-pink-100',
+            description     : 'Formation technique en informatique industrielle couvrant l\'automatisation, la programmation de systèmes embarqués, la maintenance informatique et l\'intégration de solutions technologiques dans l\'environnement industriel. '
         }
     ]
 
