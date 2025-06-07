15395a2e7ac64da39d4a6bf5dc1fbea6863b9d4e
Nathan-Sasa <Jonathanbikuta05@gmail.com>
2025-06-07 22:49:16 +0100

    nouvelle direction pour la section skill

---
+++ src/app/components/about/skills/skill-animation.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SkillAnimationDirective } from './skill-animation.directive';
+
+describe('SkillAnimationDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SkillAnimationDirective();
+    expect(directive).toBeTruthy();
+  });
+});
--- src/app/components/about/skills/skills.component.ts
+++ src/app/components/about/skills/skills.component.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+// import {  } from '../../../directives/intersection-animation.directive';
+import { SkillAnimationDirective } from './skill-animation.directive';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  imports: [CommonModule, SkillAnimationDirective],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css'
 })
