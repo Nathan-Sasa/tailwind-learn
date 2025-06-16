e0b077f0f7c08cd9b03d90cc133d518ff69b0afa
Nathan-Sasa <Jonathanbikuta05@gmail.com>
2025-06-16 20:32:29 +0100

    intregration du carousel autonime et button slide avec fade animation

--- src/app/components/projet/projet.component.ts
+++ src/app/components/projet/projet.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { fadeInDelay, slideInDown, slideInUpLong } from '../../shared/animations/animations';
 import { RouterModule } from '@angular/router';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-projet',
@@ -11,8 +12,41 @@ import { RouterModule } from '@angular/router';
   styleUrl: './projet.component.css',
   animations: [slideInDown, fadeInDelay, slideInUpLong]
 })
-export class ProjetComponent {
-    project = {
+export class ProjetComponent implements OnInit, OnDestroy{
 
+    currentIndex = 0;
+    intervalId: any;
+    slides = [
+        {
+            imgSlide : 'https://i.ibb.co/ksCGX4bc/home-capture-mobile-2.png',
+            buttonSlide: 'Mobile',
+            classSlide: 'w-52 || max-md:w-32'
+        },
+        {
+            imgSlide :'https://i.ibb.co/PsQXVcYf/home-capture-tablette.png',
+            buttonSlide: 'Tablette',
+            classSlide: 'w-110'
+        },
+        {
+            imgSlide: 'https://i.ibb.co/w8jhvLp/about-laptop.png',
+            buttonSlide: 'Pc',
+            classSlide: 'w-200'
+        }
+
+    ]
+
+    ngOnInit(): void {
+        this.startAutoSlide();
+    }
+    ngOnDestroy(): void {
+        clearInterval(this.intervalId);
+    }
+    goToSlide(index: number): void{
+        this.currentIndex = index
+    }
+    startAutoSlide(): void {
+        this.intervalId = setInterval(() => {
+            this.currentIndex = (this.currentIndex + 1) % this.slides.length;
+        }, 5000);
     }
 }
