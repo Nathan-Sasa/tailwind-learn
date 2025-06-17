8dbd92f3612c999bbd04c492128fa6def91df490
Nathan-Sasa <Jonathanbikuta05@gmail.com>
2025-06-17 10:03:45 +0100

    deplacemant du dossier assets src/app/assets vers src/assets

--- src/app/components/projet/only-projets/only-projets.component.ts
+++ src/app/components/projet/only-projets/only-projets.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-only-projets',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './only-projets.component.html',
   styleUrl: './only-projets.component.css'
 })
 export class OnlyProjetsComponent {
 
-    Project = [
+    project = [
         {
             projectName : 'Market Website',
             projectImage: '/tailwind-learn/src/app/assets/img/project/market-list.png',
