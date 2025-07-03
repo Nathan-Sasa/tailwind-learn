580eaf2cf6d2a41a27f928bc4959f2908145b7d2
Nathan-Sasa <Jonathanbikuta05@gmail.com>
2025-07-03 19:28:23 +0100

    add stacks component

--- src/app/components/about/stacks/stacks.component.ts
+++ src/app/components/about/stacks/stacks.component.ts
@@ -1,11 +1,65 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-stacks',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './stacks.component.html',
   styleUrl: './stacks.component.css'
 })
 export class StacksComponent {
 
+
+    isStackInfo = false;
+    currentIndex = 0;
+
+    @Input() isVisible = false;
+    @Output() closed = new EventEmitter<void>() 
+    
+    Onclose(){
+        this.closed.emit();
+    }
+
+    constructor(private renderer: Renderer2){}
+
+    stackOpen(){
+        this.isStackInfo = !this.isStackInfo;
+        this.updateBodyScroll();
+    }
+    closeMenu(){
+        this.isStackInfo = false;
+        this.updateBodyScroll();
+    }
+
+    updateBodyScroll(){
+        if(this.isStackInfo){
+            this.renderer.addClass(document.body, 'overflow-hidden')
+        }else{
+            this.renderer.removeClass(document.body, 'overflow-hidden')
+        }
+    }
+
+
+    stacks = [
+        {
+            name: 'HTML',
+            icon: 'ri-html5-line',
+            description: 'Langage de balisage standard pour créer des pages web.',
+            class: 'text-orange-500 group-hover:text-orange-600'
+        },
+        {
+            name: 'CSS',
+            icon: 'ri-css3-line',
+            description: 'Langage de style utilisé pour décrire la présentation des documents HTML.',
+            class: 'text-blue-500 group-hover:text-blue-600'
+        },
+    ]
+
+    indexStack = this.stacks.map((stack, index) => ({
+        name: stack.name,
+        icon: stack.icon,
+        description: stack.description,
+        class: stack.class,
+        index: index
+    }));
 }
