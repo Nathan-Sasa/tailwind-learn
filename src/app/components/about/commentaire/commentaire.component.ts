155c0cbf4b339e3a67137a22bae89665276ec335
Nathan-Sasa <Jonathanbikuta05@gmail.com>
2025-07-06 23:32:15 +0100

    add component: commentaire.component.ts

---
+++ src/app/components/about/commentaire/commentaire.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-commentaire',
+  imports: [],
+  templateUrl: './commentaire.component.html',
+  styleUrl: './commentaire.component.css'
+})
+export class CommentaireComponent {
+
+}
